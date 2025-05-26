4c4554f9e1bc5fe81942b7852bef6ee23bd26d66
Lacheln <psionict@naver.com>
2025-05-26 10:32:37 +0900

    백엔드로 입력 데이터 전송

--- frontend/src/apis/userApi.js
+++ frontend/src/apis/userApi.js
@@ -1,10 +1,14 @@
+import axios from "axios";
 
-import axios from 'axios'
+axios.defaults.withCredentials = true; // 모든 요청에 대해 withCredentials 설정
+const API_URL = import.meta.env.VITE_BACK_URL || "http://localhost:3000";
 
-axios.defaults.withCredentials = true  // 모든 요청에 대해 withCredentials 설정
-const API_URL = import.meta.env.VITE_BACK_URL||'http://localhost:3000'
+export const registerUser = async (userData) => {
+    const resonse = await axios.post(`${API_URL}/register`, userData);
+    return resonse.data;
+};
 
-export const registerUser = async (userData)=>{
-    const resonse = await axios.post(`${API_URL}/register`,userData)
-    return resonse.data
-}
+export const loginUser = async (credentials) => {
+    const response = await axios.post(`${API_URL}/login`, credentials);
+    return response.data;
+};
