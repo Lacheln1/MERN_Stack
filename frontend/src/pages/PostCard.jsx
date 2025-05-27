789a29d3207b72cc1aa586dc291db8c3ebb11786
Lacheln <psionict@naver.com>
2025-05-27 15:03:21 +0900

    좋아요 기능이 새로고침 후에도 상태를 유지하는 문제를 해결하기 위해  redux store 활용

--- frontend/src/pages/PostCard.jsx
+++ frontend/src/pages/PostCard.jsx
@@ -2,11 +2,27 @@ import React, { useState } from "react";
 import css from "./PostCard.module.css";
 import { Link, useNavigate } from "react-router-dom";
 import { toggleLike } from "../apis/postApi";
+import { useSelector } from "react-redux";
 
 const PostCard = ({ post }) => {
     const navigate = useNavigate();
+    const user = useSelector((state) => state.user.user);
+    const userId = user?.id; //현재 로그인한 사용자의 id
+
+    //초기상태를 설정할 때 로그인한 사용자가 이미 좋아요를 눌렀는지 확인
     const [isLiked, setIsLiked] = useState(false);
     const [likesCount, setLikesCount] = useState(post.likes ? post.likes.length : 0);
+
+    // 컴포넌트 마운트 시 및 post나 userId가 변경될 때 좋아요 상태를 확인
+    useEffect(() => {
+        if (userId && post.likes) {
+            // 사용자가 로그인 상태이고, 게시물에 좋아요 배열이 있을 경우
+            const userLiked = post.likes.includes(userId);
+            setIsLiked(userLiked);
+        } else {
+            setIsLiked(false);
+        }
+    }, [post, userId]);
 
     const goDetail = () => {
         navigate(`/detail/${post._id}`);
@@ -36,7 +52,7 @@ const PostCard = ({ post }) => {
         }
     };
     return (
-        <article className={css.postCard}>
+        <article className={css.postCard} onClick={goDetail}>
             <div className={css.post_img}>
                 <img src={`${import.meta.env.VITE_BACK_URL}/${post.cover}`} alt={post.title} />
             </div>
@@ -44,8 +60,12 @@ const PostCard = ({ post }) => {
 
             <div className={css.info}>
                 <p>
-                    <Link to={`/mypage`} className={css.author}>
-                        somy
+                    <Link
+                        to={`/mypage/${post.author}`}
+                        onClick={handleAuthorClick}
+                        className={css.author}
+                    >
+                        {post.author}
                     </Link>
                     <time className={css.date}>{formatDate(post.createdAt)}</time>
                 </p>
