19e72e540b160d1ca49978aa0827cbd3276e75af
Lacheln <psionict@naver.com>
2025-05-27 11:25:07 +0900

    스크롤 바가 브라우저 하단에 도달 전 3개를 추가해서 무한 스크롤 적용하기

--- frontend/src/pages/PostListPage.jsx
+++ frontend/src/pages/PostListPage.jsx
@@ -4,19 +4,46 @@ import PostCard from "./PostCard";
 import { useState } from "react";
 import { useEffect } from "react";
 import { getPostList } from "../apis/postApi";
+import { useRef } from "react";
+import { useCallback } from "react";
 const PostListPage = () => {
     const [postList, setPostList] = useState([]);
     const [isLoading, setIsLoading] = useState(true);
     const [error, setError] = useState(null);
 
+    //페이지네이션을 위한 상태 추가
+    const [page, setPage] = useState(0);
+    const [hasMore, setHasMore] = useState(true);
+    const listRef = useRef(null);
+    const observer = useRef();
+
+    //마지막 게시물 요소를 감지하는 ref 콜백
+    const lastPostElementRef = useCallback(
+        (node) => {
+            if (isLoading || !node) return;
+            if (observer.current) observer.current.disconnect();
+
+            observer.current = new IntersectionObserver((entries) => {
+                if (entries[0].isIntersecting && hasMore) {
+                    setPage((prev) => prev + 1);
+                }
+            });
+            observer.current.observe(node);
+        },
+        [isLoading, hasMore]
+    );
+
     useEffect(() => {
         const fetchPostList = async () => {
             try {
-                setIsLoading(true);
+                //페이지가 0보다 크면 로딩
+                if (page > 0) setIsLoading(true);
+
+                //수정된 페이지 정보 전달
                 const data = await getPostList();
                 console.log("목록 조회 성공:", data);
-
-                setPostList(data.posts);
+                setPostList((prev) => (page === 0 ? data.posts : [...prev, ...data.posts]));
+                setHasMore(data.hasMore);
             } catch (error) {
                 console.log("목록 조회 실패", error);
                 setError("글 목록을 불러오는 데 실패했습니다");
